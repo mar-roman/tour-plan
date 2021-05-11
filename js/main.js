8de8e4fc114249662a63734f2385e91cc80907b5
mar-roman <roman.markaryants@yandex.ru>
2021-05-11 19:03:31 +0300

    Create: booking and added map to hotel section

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
+// Switch slides with arrows
 $(document).keydown(function(e){
 if (e.which == 37) {
      $('.slider-button--prev')[0].click();
