e6181cec06dbd3b8c7b669efca4a46b9dad4339b
mar-roman <roman.markaryants@yandex.ru>
2021-05-21 00:54:21 +0300

    Create: mobile menu

--- js/main.js
+++ js/main.js
@@ -33,5 +33,5 @@ $('.newsletter').parallax({imageSrc: '../img/newsletter-bg.jpg'});
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function(){
   console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle(".navbar-bottom--visited")
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
 });
